136861c6133dbaa1ee60573d621514a438ee50e0
Thomas Hansen <31554741+thomas992@users.noreply.github.com>
2022-09-12 16:43:54 -0400

    how I do formatting
    I read left to right, I like to shorten the script as much as possible. I want to make a library that makes one user interface format for both HTML DOM and Cosmic WASM. I hope to mirror functionality to that of Mithril JS

--- ui/examples/counter.zig
+++ ui/examples/counter.zig
@@ -1,83 +1,38 @@
-const std = @import("std");
-const builtin = @import("builtin");
-const IsWasm = builtin.target.isWasm();
-const stdx = @import("stdx");
-const platform = @import("platform");
-const graphics = @import("graphics");
-const Color = graphics.Color;
-const ui = @import("ui");
-const w = ui.widgets;
+const std = @import("std"); const builtin = @import("builtin");
+const IsWasm = builtin.target.isWasm(); const stdx = @import("stdx");
+const platform = @import("platform"); const graphics = @import("graphics");
+const Color = graphics.Color; const ui = @import("ui"); const w = ui.widgets;
 
-const helper = @import("helper.zig");
-const log = stdx.log.scoped(.main);
+const helper = @import("helper.zig"); const log = stdx.log.scoped(.main);
 
-pub const App = struct {
-    counter: u32,
-
-    pub fn init(self: *App, _: *ui.InitContext) void {
-        self.counter = 0;
-    }
-
+pub const App = struct { counter: u32,
+    pub fn init(self: *App, _: *ui.InitContext) void { self.counter = 0; }
     pub fn build(self: *App, c: *ui.BuildContext) ui.FrameId {
-        const S = struct {
-            fn onClick(self_: *App, _: ui.MouseUpEvent) void {
-                self_.counter += 1;
-            }
-        };
+        const S = struct { fn onClick(self_: *App, _: ui.MouseUpEvent) void {
+                self_.counter += 1;}};
         return w.Center(.{}, 
             w.Row(.{}, &.{
-                w.Padding(.{
-                    .padding = 10,
-                    .pad_left = 30,
-                    .pad_right = 30,
-                }, 
-                    w.Text(.{
-                        .text = c.fmt("{}", .{self.counter}),
-                        .color = Color.White,
-                    }),
-                ),
-                w.TextButton(.{
-                    .text = "Count",
-                    .onClick = c.funcExt(self, S.onClick),
-                    .corner_radius = 10,
-                }),
-            }),
-        );
-    }
-};
+                w.Padding(.{ .padding = 10, .pad_left = 30, .pad_right = 30 }, 
+                    w.Text(.{ .text = c.fmt("{}", .{self.counter}), .color = Color.White})),
+                w.TextButton(.{ .text = "Count", .onClick = c.funcExt(self, S.onClick),
+                    .corner_radius = 10 }),}),);}};
 
 var app: helper.App = undefined;
 
-pub fn main() !void {
-    // This is the app loop for desktop. For web/wasm see wasm exports below.
-    try app.init("Counter");
-    defer app.deinit();
-    app.runEventLoop(update);
-}
+pub fn main() !void { // This is the app loop for desktop. For web/wasm see wasm exports below.
+    try app.init("Counter"); defer app.deinit(); app.runEventLoop(update); }
 
-fn update(delta_ms: f32) void {
-    const S = struct {
-        fn buildRoot(_: void, c: *ui.BuildContext) ui.FrameId {
-            return c.build(App, .{});
-        }
-    };
+fn update(delta_ms: f32) void { const S = struct {
+        fn buildRoot(_: void, c: *ui.BuildContext) ui.FrameId { return c.build(App, .{}); } };
     const ui_width = @intToFloat(f32, app.win.getWidth());
     const ui_height = @intToFloat(f32, app.win.getHeight());
-    app.ui_mod.updateAndRender(delta_ms, {}, S.buildRoot, ui_width, ui_height) catch unreachable;
-}
+    app.ui_mod.updateAndRender(delta_ms, {}, S.buildRoot, ui_width, ui_height) catch unreachable; }
 
 pub usingnamespace if (IsWasm) struct {
-    export fn wasmInit() [*]const u8 {
-        return helper.wasmInit(&app, "Counter");
-    }
+    export fn wasmInit() [*]const u8 { return helper.wasmInit(&app, "Counter"); }
 
     export fn wasmUpdate(cur_time_ms: f64, input_buffer_len: u32) [*]const u8 {
-        return helper.wasmUpdate(cur_time_ms, input_buffer_len, &app, update);
-    }
+        return helper.wasmUpdate(cur_time_ms, input_buffer_len, &app, update); }
 
-    /// Not that useful since it's a long lived process in the browser.
-    export fn wasmDeinit() void {
-        app.deinit();
-        stdx.wasm.deinit();
-    }
+    export fn wasmDeinit() void { app.deinit(); stdx.wasm.deinit(); } 
 } else struct {};
